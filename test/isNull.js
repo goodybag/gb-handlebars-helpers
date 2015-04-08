5d99b54ff5f6fe5689c245bccac987ac1a8e535b
Preston Pham <prestonp08@gmail.com>
2015-04-08 17:12:07 -0500

    add script to generate helpers, add isNull helper

---
+++ test/isNull.js
@@ -0,0 +1,18 @@
+var helpers = require('../lib/helpers');
+var assert = require('assert');
+var Hbs = require('handlebars');
+
+describe('isNull', function() {
+  before(function() {
+    helpers.register(Hbs, 'isNull');
+  });
+
+  it('should handle isNull', function() {
+    var str = '{{#isNull obj}}a{{else}}b{{/isNull}}';
+    var template = Hbs.compile(str);
+    assert(template({ obj: true }) === 'b');
+    assert(template({ obj: false }) === 'b');
+    assert(template({ obj: null }) === 'a');
+  });
+});
+
