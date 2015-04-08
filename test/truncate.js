14d4cb0e075bb8ef952d8ed28242431bc1682ce8
Preston Pham <prestonp08@gmail.com>
2015-04-08 17:57:35 -0500

    fix truncate

--- test/truncate.js
+++ test/truncate.js
@@ -9,11 +9,19 @@ describe('truncate', function() {
 
   it('should truncate', function() {
     var result = Hbs.helpers.truncate('abcdefghijkl', 4);
+    console.log(result);
     assert(result === 'abcd...');
   });
 
   it('should truncate with custom delimiter', function() {
     var result = Hbs.helpers.truncate('coffee barista fair-trade', 5, '*');
     assert(result === 'coffe*');
+  });
+
+  it('should truncate in a template', function() {
+    var str = '{{truncate "cat dog fish bird" 6}}';
+    var template = Hbs.compile(str);
+    console.log(template());
+    assert(template() === 'cat do...');
   });
 });
