f0aca9e9f8e6ce651b6572926e8a9f9d0d47033f
Preston Pham <prestonp08@gmail.com>
2015-04-08 15:36:22 -0500

    is helper

---
+++ test/is.js
@@ -0,0 +1,17 @@
+var helpers = require('../lib/helpers');
+var assert = require('assert');
+var Hbs = require('handlebars');
+
+describe('is', function() {
+  before(function() {
+    helpers.register(Hbs, 'is');
+  });
+
+  it('should handle is', function() {
+    var group = 'client';
+    var str = "{{#is mood 'happy' 'excited' }}Satisfactory{{else}}Unsatisfactory{{/is}}"
+    var template = Hbs.compile(str);
+    assert(template({ mood: 'happy' }) === 'Satisfactory');
+    assert(template({ mood: 'sad' }) === 'Unsatisfactory');
+  });
+});
